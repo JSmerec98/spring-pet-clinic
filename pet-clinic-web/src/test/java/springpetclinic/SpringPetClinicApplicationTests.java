d2c6790371eb97075dc23ffa0fe23c83b9f4d19f
JSmerec98 <jasiek.smerecki@gmail.com>
2020-07-25 20:21:56 +0200

    Upgrading poms.

--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,13 +1,16 @@
 package springpetclinic;
 
+import jan.springspringpetclinic.SpringPetClinicApplication;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = SpringPetClinicApplication.class)
 class SpringPetClinicApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
