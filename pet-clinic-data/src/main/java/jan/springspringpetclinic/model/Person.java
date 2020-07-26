9a503c15bd991c8e79192c7f88f9aeec0be9b682
JSmerec98 <jasiek.smerecki@gmail.com>
2020-07-26 11:10:18 +0200

    Added base entity. Closes #10.

--- pet-clinic-data/src/main/java/jan/springspringpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/jan/springspringpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package jan.springspringpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/jan/springspringpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/jan/springspringpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package jan.springspringpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
 
     private String name;
 
